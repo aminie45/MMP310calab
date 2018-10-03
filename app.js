b6eeb591c438b1b0a31c06c4f227360f88416a4f
aminie45 <aminie45@gmail.com>
2018-10-03 17:32:01 -0400

    fix the errors again after we push

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const favMovieTitleError = document.getElementById ('favMovieTitle-error');
 const verbInput = document.getElementById ('verb');
 const verbError = document.getElementById ('verb-error');
 
-<<<<<<< HEAD
 submitButton.onclick = function () {
 
             // If an error if no is made
@@ -36,7 +35,8 @@ submitButton.onclick = function () {
           " parachute." + " All my friends said I was crazy because they were using a " 
     + favFoodInput.value + 
           " parachute. We all " 
-    + verbInput.value + " out the plane……now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " 
+    + verbInput.value + " out the plane....." + 
+              "now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " 
     + favMovieTitleInput.value + " parachute!";
     
         storyP.textContent = story;
@@ -47,10 +47,8 @@ submitButton.onclick = function () {
     }
 };
 
-//-----------------MADLIB 2---------------------------//
-=======
+
 //-----------------MADLIB  TWO-------------------------------------//
->>>>>>> c827fa99cbbb8bb66a62015a585aea3e70f85a5e
 const submit2Button = document.getElementById('submit2');
 const story2P = document.getElementById('story2');
 
@@ -66,8 +64,27 @@ const favouriteSportError = document.getElementById('favouriteSport-error');
 const iceCreamInput = document.getElementById('iceCream');
 const iceCreamError = document.getElementById('iceCream-error');
 
-<<<<<<< HEAD
-=======
+submit2Button.onclick = function () {
+
+    if (!oddNumberInput.value) {
+        oddNumberError.textContent = "Please enter a Odd Number";
+    } else if (!shoeSizeInput.value) {
+        shoeSizeError.textContent = "Please enter a Shoe Size";
+    } else if (!favouriteSportInput.value) {
+        favouriteSportError.textContent = "Please enter a Sport";
+    } else if (!iceCreamInput.value) {
+        iceCreamError.textContent = "Please enter an Ice Cream Flavour";
+    } else {
+
+
+        const story2 = "The keyboard has" + oddNumberInput.value + " odd numbers." + shoeSizeInput.value + " has no value to your life & playing " + favouriteSportInput.value + " online isn't real sports. none the less, your NOT good at it so STOP!!! Turn the console off, go sit in a corner & eat some " + iceCreamInput;
+
+        story2P.textContent = story
+
+
+    }
+};
+
 //-----------------MADLIB  THREE-------------------------------------//
 const submit3Button = document.getElementById('submit3');
 const story3P = document.getElementById('story3');
@@ -84,74 +101,6 @@ const ingredientThreeError = document.getElementById('ingredientThree-error');
 const ingredientFourInput = document.getElementById('ingredientFour');
 const ingredientFourError = document.getElementById('ingredientFour-error');
 
-submitButton.onclick = function () {
-
-    // check to make sure user input a noun
-
-    if (!nounInput.value) {
-        nounError.textContent = "Please enter a NOUN";
-    } else if (!verbInput.value) {
-        verbError.textContent = "Please enter a VERB";
-    } else if (!adjectiveInput.value) {
-        adjectiveError.textContent = "Please enter a ADJECTIVE";
-    } else if (!placeInput.value) {
-        placeError.textContent = "Please enter a PLACE";
-    } else {
-
-
-        const story = "One day I went skydiving with my " +
-            nounInput.value +
-            " parachute. All my friends said I was crazy because they were using a " +
-            verbInput.value +
-            " parachute. We all " +
-            placeInput.value + " out the plane……now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " +
-            adjectiveInput.value +
-            " parachute!";
-        storyP.textContent = story;
-
-
-
-    }
-
-    document.getElementById('container').style.display = 'none';
-    document.getElementById('container2').style.display = 'block';
-    document.getElementById('container3').style.display = 'block';
-
-};
-
-
->>>>>>> c827fa99cbbb8bb66a62015a585aea3e70f85a5e
-submit2Button.onclick = function () {
-
-    if (!oddNumberInput.value) {
-        oddNumberError.textContent = "Please enter a Odd Number";
-    } else if (!shoeSizeInput.value) {
-        shoeSizeError.textContent = "Please enter a Shoe Size";
-    } else if (!favouriteSportInput.value) {
-        favouriteSportError.textContent = "Please enter a Sport";
-    } else if (!iceCreamInput.value) {
-        iceCreamError.textContent = "Please enter an Ice Cream Flavour";
-    } else {
-
-
-<<<<<<< HEAD
-        const story = "The keyboard has" + oddNumberInput.value + " odd numbers." + shoeSizeInput.value + "has no value to your life" + favouriteSportInput.value + " your NOT good at it so STOP!!!" + iceCreamInput;
-=======
-        const story = "One fly fue mdnsadlas danss " +
-            nounInput.value +
-            " parachute. asdkajsdsadj kjas dsing a " +
-            verbInput.value +
-            " parachute. We all " +
-            placeInput.value + " out the plane……now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " +
-            adjectiveInput.value +
-            " parachute!";
-        storyP.textContent = story
-
-
-
-    }
-};
-
 
 submit3Button.onclick = function () {
 
@@ -166,7 +115,7 @@ submit3Button.onclick = function () {
     } else {
 
 
-        const story = "One fly fue mdnsadlas danss " +
+        const story3 = "One fly fue mdnsadlas danss " +
             ingredientOneInput.value +
             " paraZmxzmls;cs ckasjcs " +
             ingredientTwoInput.value +
@@ -174,10 +123,9 @@ submit3Button.onclick = function () {
             ingredientThreeInput.value + "  hxjashc acascj ascha hasx satheir funerals. So, who wants to go skydiving with me this weekend? Bring your " +
             ingredientFourInput.value +
             " parachute!";
-        storyP.textContent = story
+        
+        storyP.textContent = story;
 
-
->>>>>>> c827fa99cbbb8bb66a62015a585aea3e70f85a5e
 
     }
     
