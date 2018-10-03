b303a2b6a836ae93e06e64166328843dade8a586
aminie45 <aminie45@gmail.com>
2018-10-03 19:16:30 -0400

    fix error with all files

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 const submitButton = document.getElementById('submit');
 const storyP = document.getElementById('story');
 
-const favShoeBrandInput = document.getElementById ('favShoeBrand');
-const favShoeBrandError = document.getElementById ('favShoeBrand-error');
+const favShoeBrandInput = document.getElementById('favShoeBrand');
+const favShoeBrandError = document.getElementById('favShoeBrand-error');
 
-const favFoodInput = document.getElementById ('favFood');
-const favFoodError = document.getElementById ('favFood-error');
+const favFoodInput = document.getElementById('favFood');
+const favFoodError = document.getElementById('favFood-error');
 
-const favMovieTitleInput = document.getElementById ('favMovieTitle');
-const favMovieTitleError = document.getElementById ('favMovieTitle-error');
+const favMovieTitleInput = document.getElementById('favMovieTitle');
+const favMovieTitleError = document.getElementById('favMovieTitle-error');
 
-const verbInput = document.getElementById ('verb');
-const verbError = document.getElementById ('verb-error');
+const verbInput = document.getElementById('verb');
+const verbError = document.getElementById('verb-error');
 
 submitButton.onclick = function () {
 
-            // If an error if no is made
-    
+    // If an error if no is made
+
     if (!favShoeBrandInput.value) {
         favShoeBrandError.textContent = "Please enter your Fav Shoe Brand";
-        
+
     } else if (!favFoodInput.value) {
         favFoodError.textContent = "Please enter your Fav Food";
-        
+
     } else if (!favMovieTitleInput.value) {
         favMovieTitleError.textContent = "Please enter your Fav Movie Title";
-        
+
     } else if (!verbInput.value) {
         verbError.textContent = "Please enter a VERB";
     } else {
-        
-        const story = "One day I went skydiving with my " 
-    + favShoeBrandInput.value + 
-          " parachute." + " All my friends said I was crazy because they were using a " 
-    + favFoodInput.value + 
-          " parachute. We all " 
-    + verbInput.value + " out the plane....." + 
-              "now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " 
-    + favMovieTitleInput.value + " parachute!";
-    
+
+        const story = "One day I went skydiving with my " +
+            favShoeBrandInput.value +
+            " parachute." + " All my friends said I was crazy because they were using a " +
+            favFoodInput.value +
+            " parachute. We all " +
+            verbInput.value + " out the plane....." +
+            "now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " +
+            favMovieTitleInput.value + " parachute!";
+
         storyP.textContent = story;
-        
-    
-    document.getElementById('container2').style.display = 'block';
-    //document.getElementById('container').style.display = 'none';
+
+
+        document.getElementById('container2').style.display = 'block';
+        document.getElementById('container').style.display = 'none';
     }
 };
 
@@ -77,11 +77,12 @@ submit2Button.onclick = function () {
     } else {
 
 
-        const story2 = "The keyboard has" + oddNumberInput.value + " odd numbers." + shoeSizeInput.value + " has no value to your life & playing " + favouriteSportInput.value + " online isn't real sports. none the less, your NOT good at it so STOP!!! Turn the console off, go sit in a corner & eat some " + iceCreamInput;
+        const story2 = "The keyboard has " + oddNumberInput.value + " odd numbers. " + shoeSizeInput.value + " has no value to your life & playing " + favouriteSportInput.value + " online isn't real sports. None the less, your NOT good at it so STOP!!! Turn the console off, go sit in a corner & eat some " + iceCreamInput.value + ".";
 
-        story2P.textContent = story
+        story2P.textContent = story2;
 
-
+        document.getElementById('container3').style.display = 'block';
+        document.getElementById('container2').style.display = 'none';
     }
 };
 
@@ -115,22 +116,19 @@ submit3Button.onclick = function () {
     } else {
 
 
-        const story3 = "One fly fue mdnsadlas danss " +
+        const story3 = "Wash the  " +
             ingredientOneInput.value +
-            " paraZmxzmls;cs ckasjcs " +
+            " slices of chicken with soap to make sure it's clean. " + "Add the 5 lbs of " +
             ingredientTwoInput.value +
-            " parachute. We all " +
-            ingredientThreeInput.value + "  hxjashc acascj ascha hasx satheir funerals. So, who wants to go skydiving with me this weekend? Bring your " +
+            " to make it spicy " + "Place the chicken in the oven at Temp " +
+            ingredientThreeInput.value + "  After 2 days in the oven, remove & eat " + "If you get using " +
             ingredientFourInput.value +
-            " parachute!";
-        
-        storyP.textContent = story;
+            " thats all on you, you dummy!!!";
 
+        story3P.textContent = story3;
 
+        document.getElementById('container3').style.display = 'none';
     }
-    
+
 
 };
-
-    
-    
