aa03d81e3d85acf7781e4ed4f815ec1353e52b7b
aminie45 <aminie45@gmail.com>
2018-10-03 13:19:50 -0400

    up date the first story & style

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 const submitButton = document.getElementById ('submit');
 const storyP = document.getElementById ('story');
 
-const nounInput = document.getElementById ('noun');
-const nounError = document.getElementById ('noun-error');
+const favShoeBrandInput = document.getElementById ('favShoeBrand');
+const favShoeBrandError = document.getElementById ('favShoeBrand-error');
+
+const favFoodInput = document.getElementById ('favFood');
+const favFoodError = document.getElementById ('favFood-error');
+
+const favMovieTitleInput = document.getElementById ('favMovieTitle');
+const favMovieTitleError = document.getElementById ('favMovieTitle-error');
 
 const verbInput = document.getElementById ('verb');
 const verbError = document.getElementById ('verb-error');
-
-const adjectiveInput = document.getElementById ('adjective');
-const adjectiveError = document.getElementById ('adjective-error');
-
-const placeInput = document.getElementById ('place');
-const placeError = document.getElementById ('place-error');
 
 submitButton.onclick = function() {
     
     // check to make sure user input a noun
     
-    if (!nounInput.value) {
-        nounError.textContent = "Please enter a NOUN";
+    if (!favShoeBrandInput.value) {
+        favShoeBrandError.textContent = "Please enter your Fav Shoe Brand";
+    } else if (!favFoodInput.value) {
+        favFoodError.textContent = "Please enter your Fav Food";
+    } else if (!favMovieTitleInput.value) {
+        favMovieTitleError.textContent = "Please enter your Fav Movie Title";
     } else if (!verbInput.value) {
         verbError.textContent = "Please enter a VERB";
-    } else if (!adjectiveInput.value) {
-        adjectiveError.textContent = "Please enter a ADJECTIVE";
-    } else if (!placeInput.value) {
-        placeError.textContent = "Please enter a PLACE";
     } else {
-        const story = "Once there was a " 
-    + nounInput.value + 
-          " named Jerry." + " He loves to " 
-    + verbInput.value + 
-          " really fast in the summer. His fav place to do this is " 
-    + placeInput.value + ". this was too " 
-    + adjectiveInput.value + 
-          " for me so i didn't go with him.";
+        
+        const story = "One day I went skydiving with my " 
+    + favShoeBrandInput.value + 
+          " parachute." + " All my friends said I was crazy because they were using a " 
+    + favFoodInput.value + 
+          " parachute. We all " 
+    + verbInput.value + " out the plane……now I need new friends & a suit to wear to their funerals. So, who wants to go skydiving with me this weekend? Bring your " 
+    + favMovieTitleInput.value + 
+          " parachute!";
     
     storyP.textContent = story;
         }
